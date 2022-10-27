c68e8a42c5ab17429ff0e44b2f6787593dcc5899
buhaoqi <yeszhangjie@gmail.com>
2022-10-27 14:47:17 +0800

    fadeNumbers

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ JS练习题
 - [百度视频排行榜](https://buhaoqi.github.io/js-exercises/function-baidu-video-top8/index.html)
 - [回滚的圆](https://buhaoqi.github.io/js-exercises/function-forwards-and-backwards/index.html)
 - [上下运动的圆](https://buhaoqi.github.io/js-exercises/function-up-and-down/index.html)
-- [消失的数字](https://buhaoqi.github.io/js-exercises/function-fadeOutTxt/index.html)
+- [消失的数字](https://buhaoqi.github.io/js-exercises/function-fadeNumbers/index.html)
+- [封装opacity函数](https://buhaoqi.github.io/js-exercises/function-opacity/index.html)
 
 ## Date对象
 - [输出系统时间](https://buhaoqi.github.io/js-exercises/date-currentTime/currentTime.html)
--- assets/js/kquery.js
+++ assets/js/kquery.js
@@ -4,21 +4,41 @@ function getStyle(obj,attr){
 }
 
 //moveFn() ele:元素 attr:长度属性 step:步长 target:目标点 callback: 回调函数
-function moveFn(ele, attr, step, target, callback) {
-    //2.变量
-    let l = parseInt(getStyle(ele, attr))
-    step = l < target ? step : -step
-    clearInterval(ele.timer)
-    ele.timer = window.setInterval(function () {
-        l = parseInt(getStyle(ele, attr)) + step
-        if (l > target && step > 0 || l < target && step < 0) l = target
-        ele.style[attr] = l + 'px'
-        if (l == target) {
-            clearInterval(ele.timer)
-            // if(callback) callback() 
-            callback && callback()
-        }
-    }, 10)
+function move(obj, attr, dir, target, endFn) {
+    // console.log(1)
+    // let l = parseInt(getStyle(ele, attr))
+    // step = l < target ? step : -step
+    // clearInterval(ele.timer)
+    // ele.timer = window.setInterval(function () {
+    //     l = parseInt(getStyle(ele, attr)) + step
+    //     if (l > target && step > 0 || l < target && step < 0) l = target
+    //     ele.style[attr] = l + 'px'
+    //     if (l == target) {
+    //         clearInterval(ele.timer)
+    //         // if(callback) callback() 
+    //         callback && callback()
+    //     }
+    // }, 30)
+    dir = parseInt(getStyle( obj, attr )) < target ? dir : -dir;
+	
+	clearInterval( obj.timer );
+	
+	obj.timer = setInterval(function () {
+		
+		var speed = parseInt(getStyle( obj, attr )) + dir;
+		
+		if ( speed > target && dir > 0 ||  speed < target && dir < 0  ) {
+			speed = target;
+		}
+		
+		obj.style[attr] = speed + 'px';
+		
+		if ( speed == target ) {
+			clearInterval( obj.timer );
+			endFn && endFn();
+		}
+		
+	}, 30);
 }
 
 //double Digits
@@ -26,28 +46,42 @@ function double(n) {
     return n > 9 ? "" + n : "0" + n;
 }
 
-//
-function opacity(obj, num, target, endFn) {
-	
-    num = getStyle(obj, 'opacity')*100 < target ? num : -num;
-    
-    clearInterval( obj.opacity );
-    
-    obj.opacity = setInterval(function () {
-        
-        var speed = parseInt(getStyle(obj, 'opacity')*100) + num;
-        
-        if ( speed > target && num > 0 || speed < target && num < 0 ) {
-            speed = target;
-        }
-        
-        obj.style.opacity = speed/100;
-        // obj.style.filter = 'alpha(opacity='+ speed +')';
-        
-        if ( speed == target ) {
-            clearInterval( obj.opacity );
-            endFn && endFn();
-        }
+//透明度变化函数
+function opacity(element, step, target, callback) {
+    // step = getStyle(element, 'opacity')*100 < target ? step : -step;
+    // clearInterval(element.timer)
+    // element.timer = setInterval(function () {
 
-    }, 20);
+    //     let o = parseInt(getStyle(element, 'opacity') * 100) + step
+    //     if (o > target && step > 0 || o < target && step < 0) o = target
+
+    //     element.style.opacity = o / 100
+    //     // element.style.filter = 'alpha(opacity='+ speed +')';
+
+    //     if (o == target) {
+    //         clearInterval(element.timer)
+    //         callback && callback()
+    //     }
+    // }, 20)
+    step = getStyle(element, 'opacity')*100 < target ? step : -step;
+		
+		clearInterval( element.opacity );
+		
+		element.opacity = setInterval(function () {
+			
+			var opa = parseInt(getStyle(element, 'opacity')*100) + step;
+			
+			if ( opa > target && step > 0 || opa < target && step < 0 ) {
+				opa = target;
+			}
+			
+			element.style.opacity = opa/100;
+			// element.style.filter = 'alpha(opacity='+ opa +')';
+			
+			if ( opa == target ) {
+				clearInterval( element.opacity );
+				callback && callback();
+			}
+
+		}, 20);
 }
